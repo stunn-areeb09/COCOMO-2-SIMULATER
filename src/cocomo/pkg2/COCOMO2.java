397d5e2c3b1b4be90b03f498f781ace3d0f32e78
Syed Areeb Wadood <areeb.wadood9@gmail.com>
2019-06-19 16:12:31 +0530

    the Project Uploaded

---
+++ src/cocomo/pkg2/COCOMO2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cocomo.pkg2;
+
+/**
+ *
+ * @author AREEB
+ */
+public class COCOMO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJFrame fr = new NewJFrame();
+        fr.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
